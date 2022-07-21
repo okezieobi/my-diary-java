b15038dd0429e58e26b5dcc142508426bdddb77b
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-07-21 23:04:10 +0100

    Setup complete

---
+++ mydiary/src/main/java/mydiary/v1/mydiary/Greetings.java
@@ -0,0 +1,19 @@
+package mydiary.v1.mydiary;
+
+public class Greetings {
+    private final long id;
+    private final String content;
+
+    public Greetings(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
