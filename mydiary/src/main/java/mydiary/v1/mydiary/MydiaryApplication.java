b7efa9af1bbaf3a2bc827618b608fca53c896e81
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-07-21 16:25:32 +0100

    project setup

---
+++ mydiary/src/main/java/mydiary/v1/mydiary/MydiaryApplication.java
@@ -0,0 +1,13 @@
+package mydiary.v1.mydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydiaryApplication.class, args);
+	}
+
+}
---
+++ mydiary/src/test/java/mydiary/v1/mydiary/MydiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package mydiary.v1.mydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
